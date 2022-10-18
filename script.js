57d0ff3abc79ae3f3a7093afe59d1dcb5751c7ca
Victor-Zarzar <victorzarzar58@gmail.com>
2022-10-18 07:21:37 -0300

    ProjetoMeuSite

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing", {
-        strings:["Developer", "Front-End"],
+        strings:["Developer", "Software"],
         typeSpeed:100,
         backSpeed:60,
         loop:true
